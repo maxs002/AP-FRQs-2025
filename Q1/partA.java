05eefee7912c32914fb427a0ebb877907932fb04
maxs002 <maxshi002@gmail.com>
2025-05-13 09:37:23 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,3 +1,12 @@
 public int walkDogs(int hour){
-
+  int numDogs = company.numAvaibleDogs(hour);
+  if(maxDogs>numDogs){
+    company.updateDogs(hour,numDogs);
+    return numDogs;
+  }
+  else{
+    company.updateDogs(hour,maxDogs);
+    return maxDogs;
+  }
 }
+    
