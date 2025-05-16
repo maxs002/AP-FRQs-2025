beec02108d5a7e96c69257fe88772b50d7494382
maxs002 <maxshi002@gmail.com>
2025-05-16 07:31:35 -0400

    Update SignedText.java

--- Q2/SignedText.java
+++ Q2/SignedText.java
@@ -1,2 +1,27 @@
 public class SignedText{
+    private String firstN;
+    private String lastN;
 
+    public SignedText(String firstN,String lastN){
+        this.firstN = firstN;
+        this.lastN = lastN;
+    }
+    public String getSignature(){
+        if (firstN.equals("")){
+            return lastN;
+        }
+        else{
+            return firstN.substring(0,1) +"-"+ lastN;
+        }
+    }
+    public String addSignature(String str){
+        String signature = getSignature();
+        if(str.length()>= signature.length() && str.substring(str.length() - signature.length()).equals(signature)){
+            return str;
+        }
+        if(str.length()>=signature.length() && str.substring(0,signature.length()).equals(signature)){
+            return str.substring(signature.length()) + signature;
+        }
+        return str+ signature;
+    }
+}
