909e0fac4199d595e216ebf705dbdfffb2acae21
maxs002 <maxshi002@gmail.com>
2025-05-19 00:26:43 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,1 +1,14 @@
-
+public boolean clearPair(int rows, int cols){
+    for(int i = rows;i<puzzle.length;i++){
+        for(int j = 0;j<puzzle[i].length;j++){
+            if (!(i == rows && j == cols)){
+                if((puzzle[i][j]==puzzle[rows][cols])|| (puzzle[i][j]+puzzle[rows][cols]==10)){
+                    puzzle[i][j]= 0;
+                    puzzle[rows][cols]= 0;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
