4f3685e814ef3ac0ac0c74d390029b5b2109c85c
maxs002 <maxshi002@gmail.com>
2025-05-19 00:09:33 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,13 @@
+public ArrayList<Match> buildMatches() { 
+        ArrayList<Match> matchUp = new ArrayList<Match>(); 
+        int start = 0;
 
+        if (competitorList.size() % 2 == 1) {
+            start = 1;
+        }
+        for (int i = start; i < start + (competitorList.size() - start) / 2; i++) {
+            matchUp.add(new Match(competitorList.get(i), competitorList.get(competitorList.size()-1-(i-start))));
+        }
+    }
+    return matchUp;
+}
