4bee472764976ddb28ae4c69e27e7ab038fb2d57
maxs002 <maxshi002@gmail.com>
2025-05-19 00:18:19 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,8 @@
-
+public class SumOrSameGame(int numRows, int numCols) {
+    puzzle = new int[numRows][numCols];
+    for(int i =0; i<numRows;i++){
+        for(int j = 0; j<numCols;j++){
+            puzzle[i][j]= (int)(Math.random*9)+1
+        }
+    }
+}
